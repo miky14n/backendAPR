07bdf9382541fdb5da119f79694ca7645536f3b7
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-27 01:18:38 -0400

    Creando Repositorio a utilizar de BasicInformation

---
+++ Data/Repository/BasicInformationRepository.cs
@@ -0,0 +1,32 @@
+﻿using appPrevencionRiesgos.Data.Entities;
+
+namespace appPrevencionRiesgos.Data.Repository
+{
+    public class BasicInformationRepository : IBasicInformationRepository
+    {
+        public void CreateInformation(BasicInformationEntity basicInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteInformationAsync(int informationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BasicInformationEntity>> GetAllInformationAsync(string direction, string orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasicInformationEntity> GetOneInformationAsync(int informationId, bool showInformation = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateInformationAsync(int informationId, BasicInformationEntity basicInformation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
