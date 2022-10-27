b1b9bfb1df36d23159a47a24fa1ba3a609926fac
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-27 00:43:01 -0400

    Creando la entidad de Información Básica

--- Data/Entities/BasicInformationEntity.cs
+++ Data/Entities/BasicInformationEntity.cs
@@ -1,12 +1,12 @@
-﻿namespace appPrevencionRiesgos.Model
+﻿namespace appPrevencionRiesgos.Data.Entities
 {
-    public class BasicInformationModel
+    public class BasicInformationEntity
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string City { get; set; }
         public string Province { get; set; }
-
     }
 }
