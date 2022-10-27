438d406bfb17c45f904f768e7bb17cbfbade902d
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-27 01:17:33 -0400

    Creando interfaz BasicInformation como Repositorio

---
+++ Data/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using appPrevencionRiesgos.Data.Entities;
+using appPrevencionRiesgos.Model;
+using AutoMapper;
+
+namespace appPrevencionRiesgos.Data
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<BasicInformationEntity, BasicInformationModel>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ Data/Repository/IBasicInformationRepository.cs
@@ -0,0 +1,13 @@
+﻿using appPrevencionRiesgos.Data.Entities;
+
+namespace appPrevencionRiesgos.Data.Repository
+{
+    public interface IBasicInformationRepository
+    {
+        Task<IEnumerable<BasicInformationEntity>> GetAllInformationAsync(string direction, string orderBy);
+        Task<BasicInformationEntity> GetOneInformationAsync(int informationId, bool showInformation = false);
+        void CreateInformation(BasicInformationEntity basicInformation);
+        Task UpdateInformationAsync(int informationId, BasicInformationEntity basicInformation);
+        Task DeleteInformationAsync(int informationId);
+    }
+}
