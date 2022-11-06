6d20c10ad00af223c0cd8ebae1aa1b58df5d25c9
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-06 13:36:46 -0400

    Creando excepciones y Obtener toda la Información en el Controlador

---
+++ Controllers/BasicInformationController.cs
@@ -0,0 +1,38 @@
+﻿using appPrevencionRiesgos.Data.Repository;
+using appPrevencionRiesgos.Exceptions;
+using appPrevencionRiesgos.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace appPrevencionRiesgos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicInformationController : Controller
+    {
+        private IBasicInformationRepository dbContext = new BasicInformationRepository();
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BasicInformationModel>>> GetAllInformationAsync([FromQuery] string direction = "asc", string orderBy = "id")
+        {
+            try
+            {
+                var informationList = await dbContext.GetAllInformationAsync(direction, orderBy);
+                return Ok(informationList);
+            }
+            catch (NotFoundElementException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (InvalidElementOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Something happend.");
+            }
+        }
+
+    }
+}
---
+++ Exceptions/InvalidElementOperationException.cs
@@ -0,0 +1,10 @@
+﻿namespace appPrevencionRiesgos.Exceptions
+{
+    public class InvalidElementOperationException : Exception
+    {
+        public InvalidElementOperationException(string message) : base(message)
+        {
+            
+        }
+    }
+}
