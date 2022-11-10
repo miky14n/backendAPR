3f2979efc18ac0824cc9946257cc5380063711ad
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-09 22:37:35 -0400

    Añadiendo CRUD BasicInformationRepository

--- Data/Repository/BasicInformationRepository.cs
+++ Data/Repository/BasicInformationRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace appPrevencionRiesgos.Data.Repository
+﻿using appPrevencionRiesgos.Data.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace appPrevencionRiesgos.Data.Repository
 {
-    public class BasicInformationRepository
+    public class BasicInformationRepository : IBasicInformationRepository
     {
+        internal MongoDbContext _mongoRepository = new MongoDbContext();
+        private IMongoCollection<BasicInformationEntity> collection;
+
+        public BasicInformationRepository()
+        {
+            collection = _mongoRepository.dbContext.GetCollection<BasicInformationEntity>("BasicInformation");
+        }
+
+        public void CreateInformation(BasicInformationEntity basicInformation)
+        {
+            collection.InsertOne(basicInformation);
+        }
+
+        public async Task DeleteInformationAsync(string informationId)
+        {
+            var informationToDelete = Builders<BasicInformationEntity>.Filter.Eq(i => i.Id, new ObjectId(informationId));
+            await collection.DeleteOneAsync(informationToDelete);
+        }
+
+        public async Task<IEnumerable<BasicInformationEntity>> GetAllInformationAsync(string direction, string orderBy)
+        {
+            return await collection.FindAsync(new BsonDocument()).Result.ToListAsync();
+        }
+
+        public async Task<BasicInformationEntity> GetOneInformationAsync(string informationId, bool showInformation = false)
+        {
+            return await collection.FindAsync(new BsonDocument { { "_id", new ObjectId(informationId) } }).Result.FirstAsync();
+        }
+
+        public async Task UpdateInformationAsync(string informationId, BasicInformationEntity basicInformation)
+        {
+            var informationToUpdate = Builders<BasicInformationEntity>.Filter.Eq(i => i.Id, basicInformation.Id);
+            await collection.ReplaceOneAsync(informationToUpdate, basicInformation);
+        }
     }
 }
