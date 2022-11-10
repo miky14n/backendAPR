4bd6cb91bc9fcf720c847f9551807401a90002f9
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-09 22:41:51 -0400

    Añadiendo el Repositorio de usuario

---
+++ Data/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using appPrevencionRiesgos.Data.Entities;
+
+namespace appPrevencionRiesgos.Data.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserEntity>> GetAllUsersAsync(string direction, string orderBy);
+        Task<UserEntity> GetOneUserAsync(string userId, bool showUser = false);
+        void CreateUser(UserEntity user);
+        Task UpdateUserAsync(string userId, UserEntity user);
+        Task DeleteUserAsync(string userId);
+    }
+}
---
+++ Data/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using appPrevencionRiesgos.Data.Entities;
+
+namespace appPrevencionRiesgos.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public void CreateUser(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserEntity>> GetAllUsersAsync(string direction, string orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserEntity> GetOneUserAsync(string userId, bool showUser = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(string userId, UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
