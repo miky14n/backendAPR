88a0e763a41b3f2c88a1df804ff212b834227361
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-21 12:54:17 -0400

    Combinando datos de modelo User y Basic

--- Model/UserInformationModel.cs
+++ Model/UserInformationModel.cs
@@ -5,9 +5,12 @@ namespace appPrevencionRiesgos.Model.Security
     public class UserInformationModel
     {
         public ObjectId Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
         public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
         public string Phone { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
     }
 }
